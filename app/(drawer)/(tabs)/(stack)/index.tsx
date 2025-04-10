652a758aea56d26375cf40c6a1bf3304df8a1c10
MiloCod <milo_ainos@hotmail.com>
2025-04-10 00:59:09 -0600

    Uso de haptics, nativewind, drawer, tabs y conexion con el API

--- app/(drawer)/(tabs)/(stack)/index.tsx
+++ app/(drawer)/(tabs)/(stack)/index.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+//cspell:disable
+import { View, Text, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { useAirQuality } from "../../../../componentes/useAirQuality";
+import AirQualityCard from "../../../../componentes/AirQualityCard";
 
 const index = () => {
-  return (
-    <View>
-      <Text className='text-3xl'>index del ta</Text>
-    </View>
-  )
-}
 
-export default index
+  const city = 'Guadalupe';
+  const state = 'Nuevo Leon'
+  const country = 'Mexico';
+
+  const { data, loading } = useAirQuality(city, state, country);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Cargando datos</Text>
+        <ActivityIndicator size={"large"} />
+      </View>
+    );
+  }
+  return (
+    <View style={{ alignItems: 'center', marginTop: 15 }} >
+      <Text>Calidad del aire en {city}</Text>
+      <View style={{ alignItems: 'center', marginTop: 200 }}>
+        {data && <AirQualityCard data={data} />}
+      </View>
+
+    </View>
+  );
+};
+
+export default index;
+
