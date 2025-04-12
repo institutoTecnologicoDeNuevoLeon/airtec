d81f6d8a2836525dfd4ed790e2e30a69e983c3b8
MiloCod <milo_ainos@hotmail.com>
2025-04-11 22:40:29 -0600

    Actualizado

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,56 +1,89 @@
-//cspell:disable 
-import { View, Text } from 'react-native'
-import React from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Drawer } from 'expo-router/drawer';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import CustomerDrawerLayout from '../../componentes/CustomDrawerLayout/CustomerDrawerLayout';
-
+//cspell:disable
+import { View, Text, Touchable, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CustomerDrawerLayout from "../../componentes/CustomDrawerLayout/CustomerDrawerLayout";
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
 
 const DrawerLayout = () => {
-    return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <Drawer drawerContent={CustomerDrawerLayout}>
-                <Drawer.Screen
-                    name="(tabs)" // This is the name of the page and must match the url from root
-                    options={{
-                        drawerLabel: 'Inicio',
-                        title: 'AirTec',
-                        headerTitleAlign: 'center',
-                        drawerItemStyle: { display: 'none' },
-                        drawerIcon: ({ size, color }) => (
-                            <Ionicons name='leaf-outline'></Ionicons>
-                        )
-                    }}
-                />
-                <Drawer.Screen
-                    name="acerca/index" // This is the name of the page and must match the url from root
-                    options={{
-                        drawerLabel: 'Acerca',
-                        title: 'overview',
-                        drawerActiveTintColor: '#000',
-                        drawerActiveBackgroundColor: '#5363df',
-                        drawerIcon: ({ size, color }) => (
-                            <Ionicons name='information-outline'></Ionicons>
-                        )
-                    }}
-                />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        drawerContent={CustomerDrawerLayout}
+        screenOptions={{
+            drawerActiveBackgroundColor: "#dee2e6",
+            drawerActiveTintColor: '#023e8a',
+            headerTitleAlign: 'center',
+            headerTintColor: "#495057",
+                
+                headerStyle: {
+                    backgroundColor: "#ced4da",
+                },
+          
+                drawerStyle:{
+                    backgroundColor: '#adb5bd'
+                },
 
-                <Drawer.Screen
-                    name="contacto/index" // This is the name of the page and must match the url from root
-                    options={{
-                        drawerLabel: 'Contacto',
-                        title: 'overview',
-                        drawerActiveTintColor: '#000',
-                        drawerActiveBackgroundColor: '#5363df',
-                        drawerIcon: ({ size, color }) => (
-                            <Ionicons name='call-outline'></Ionicons>
-                        )
-                    }}
-                />
-            </Drawer>
-        </GestureHandlerRootView>
-    )
-}
+                headerTitleStyle: {
+                    fontFamily: "PTSerif-BoldItalic",
+                    fontSize: 25,
+                    letterSpacing: 5,
+                },
+                
+                headerLeft: () => {
+                    const navigation = useNavigation();
+                    return (
+                    <TouchableOpacity
+                        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer()) }
+                        style={{ marginLeft: 15 }}>
+                {/* <Image 
+                                    source={require('../../assets/images/menu-de-hamburguesas.png')}
+                                    style={{width:30, height:30 }}
+                                    /> */}
+                            <Ionicons name="grid" size={24} color={"#495057"}></Ionicons>
+              </TouchableOpacity>
+            );
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="(tabs)" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Inicio",
+            title: "AirTec",
+            // drawerItemStyle: { display: 'none' },
+            drawerIcon: ({ focused, size, color }) => (
+                <Ionicons name="home" size={20} style={{ paddingLeft: 2 }} color={focused ? '#023e8a' : '#0096c7'}></Ionicons>
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="acerca/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Acerca",
+            title: "Acerca",
+            drawerIcon: ({ focused,size, color }) => (
+                <Ionicons name="information-circle-outline" size={24} color={focused ? '#023e8a' : '#0096c7'}></Ionicons>
+            ),
+          }}
+        />
 
-export default DrawerLayout
+        <Drawer.Screen
+          name="contacto/index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: "Contacto",
+            title: "Contacto",
+            drawerIcon: ({ focused, size, color }) => (
+                <Ionicons name="call" size={20} style={{ paddingLeft: 2 }} color={focused ? '#023e8a' : '#0096c7'}></Ionicons>
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
+};
+
+export default DrawerLayout;
--- componentes/CustomDrawerLayout/CustomerDrawerLayout.tsx
+++ componentes/CustomDrawerLayout/CustomerDrawerLayout.tsx
@@ -1,15 +1,35 @@
+//cspell:disable 
 import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
 
 
 export default function CustomerDrawerLayout(props: any) {
 
 
     return (
-    <View className="flex-1" >
-    <DrawerContentScrollView {...props}>
-        <DrawerItemList {...props} />
-    </DrawerContentScrollView>
-    </View>
+        <View className="flex-1" >
+
+            <DrawerContentScrollView {...props}>
+                {/* header */}
+                <View>
+                    <Image
+                        source={require('../../assets/images/itnllogo.png')}
+                        style={{ width: 300, height: 150, borderRadius: 30, marginBottom: 20 }}
+                    />
+                </View>
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+
+
+            {/* footer */}
+            <View style={{
+                borderTopColor: '#dde3fe',
+                borderTopWidth: 1,
+                padding: 20,
+                paddingBottom: 20,
+            }}>
+                <Text>Footer</Text>
+            </View>
+        </View>
     )
 }
